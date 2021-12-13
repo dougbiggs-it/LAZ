37142e14c4431a796f15f57417be9baba9c975f1
doug biggs <douglas.biggs@learninga-z.com>
2021-12-13 10:37:10 -0500

    change for test on Jenkins for Git

---
+++ Test Suites/ts02_smoke.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts02_smoke</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9900d61d-6d44-4e00-a4f3-8ee2ca58bd95</testSuiteGuid>
+   <testCaseLink>
+      <guid>3159d653-daf5-4626-8f50-66eedc269f82</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc02_AddProductCheckOrderSummary</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
