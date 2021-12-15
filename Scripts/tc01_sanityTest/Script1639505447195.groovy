091f52885863f432ed0dffba3e63333d85604e40
doug biggs <douglas.biggs@learninga-z.com>
2021-12-15 10:05:27 -0500

    change for test on Jenkins for Git

--- Scripts/tc01_sanityTest/Script1639505447195.groovy
+++ Scripts/tc01_sanityTest/Script1639505447195.groovy
@@ -1,39 +1,30 @@
-package global
-
 import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-
-import com.kms.katalon.core.annotation.Keyword
-import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling
-import com.kms.katalon.core.testcase.TestCase
-import com.kms.katalon.core.testdata.TestData
-import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
 
-import internal.GlobalVariable
+WebUI.disableSmartWait()
 
-public class utils {
+WebUI.openBrowser('')
 
-	@Keyword (keywordObject = "Browser")
-	def loginRazPlus() {
-		// enter your code here
-		// you can use either Groovy or Java
+WebUI.maximizeWindow()
 
-		WebUI.disableSmartWait()
+WebUI.navigateToUrl('https://pre.raz-kids.com/')
 
-		WebUI.openBrowser('')
+Thread.sleep(5000)
 
-		WebUI.maximizeWindow()
-
-		WebUI.navigateToUrl('https://pre.raz-kids.com/')
-
-	}
-}
+WebUI.closeBrowser()
---
+++ Test Suites/ts01_sanitySuite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts01_sanitySuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cbc7cb51-db3f-4b52-b83f-6d7cc602a535</testSuiteGuid>
+   <testCaseLink>
+      <guid>7e09b3ac-85d0-4e54-b42d-88846f3c3267</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc01_sanityTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
